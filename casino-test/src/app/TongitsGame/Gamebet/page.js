3ccff6041279dc6e9f38611e4d761b289eb295d9
Lowey G. Ecat <118261670+LoweyGEcat@users.noreply.github.com>
2025-02-04 20:24:27 +0800

    branch created
    lowey branch

--- casino-test/src/app/TongitsGame/Gamebet/page.js
+++ casino-test/src/app/TongitsGame/Gamebet/page.js
@@ -18,33 +18,23 @@ function GameBet() {
 
   const exitGamebet = () => {
     router.push("/TongitsGame");
-  }
+  };
 
   return (
-    <div
-      className="w-full h-screen"
-      style={{
-        background:
-          "linear-gradient(to right, rgba(2, 24, 33, 0.6) 25%, rgba(149, 74, 74, 0.8) 44%)",
-        zIndex: -1,
-      }}
-    >
-      <div className="absolute w-screen h-16 top-0 bg-user-name">
-        <div className="flex flex-row h-full w-full justify-between">
-          <button onClick={exitGamebet}>
-            <Image
-              src="/image/existButton.svg"
-              alt="My image"
-              width={50}
-              height={50}
-              className="w-full h-full"
-              style={{
-                transition: "transform 0.3s ease-in-out",
-              }}
-            />
-          </button>
-          {/* <NetworkStatus /> */}
-        </div>
+    <div className="w-full h-screen bg-gradient-to-b from-[#080E81] to-[#254F64]">
+      <button onClick={exitGamebet}>
+        <Image
+          src="/image/existButton.svg"
+          alt="My image"
+          width={50}
+          height={50}
+          className="w-full h-full"
+          style={{
+            transition: "transform 0.3s ease-in-out",
+          }}
+        />
+      </button>
+      {/* <div className="absolute w-screen h-16 top-0 bg-user-name">
         <div className="w-full h-auto flex justify-center items-center">
           <div className="flex flex-col">
             <div className="flex justify-between flex-row items-center px-24 w-screen">
@@ -87,7 +77,12 @@ function GameBet() {
             </div>
             <div className="flex justify-between flex-row px-24 gap-10 w-screen">
               <div>
-                <button onClick={() => {handleClick(0); setBet(100000)}}>
+                <button
+                  onClick={() => {
+                    handleClick(0);
+                    setBet(100000);
+                  }}
+                >
                   <Image
                     src="/image/gamebet100.svg"
                     alt="My image"
@@ -100,7 +95,12 @@ function GameBet() {
                     }`}
                   />
                 </button>
-                <button onClick={() => {handleClick(1); setBet(50000)}}>
+                <button
+                  onClick={() => {
+                    handleClick(1);
+                    setBet(50000);
+                  }}
+                >
                   <Image
                     src="/image/gamebet50.svg"
                     alt="My image"
@@ -113,7 +113,12 @@ function GameBet() {
                     }`}
                   />
                 </button>
-                <button onClick={() => {handleClick(2); setBet(20000)}}>
+                <button
+                  onClick={() => {
+                    handleClick(2);
+                    setBet(20000);
+                  }}
+                >
                   <Image
                     src="/image/gamebet20.svg"
                     alt="My image"
@@ -128,7 +133,12 @@ function GameBet() {
                 </button>
               </div>
               <div>
-                <button onClick={() => {handleClick(3); setBet(2000)}}>
+                <button
+                  onClick={() => {
+                    handleClick(3);
+                    setBet(2000);
+                  }}
+                >
                   <Image
                     src="/image/gamebet2.svg"
                     alt="My image"
@@ -141,7 +151,12 @@ function GameBet() {
                     }`}
                   />
                 </button>
-                <button onClick={() => {handleClick(4); setBet(5000)}}>
+                <button
+                  onClick={() => {
+                    handleClick(4);
+                    setBet(5000);
+                  }}
+                >
                   <Image
                     src="/image/gamebet5.svg"
                     alt="My image"
@@ -154,7 +169,12 @@ function GameBet() {
                     }`}
                   />
                 </button>
-                <button onClick={() => {handleClick(5); setBet(10000)}}>
+                <button
+                  onClick={() => {
+                    handleClick(5);
+                    setBet(10000);
+                  }}
+                >
                   <Image
                     src="/image/gamebet10.svg"
                     alt="My image"
@@ -185,6 +205,26 @@ function GameBet() {
             </div>
           </div>
         </div>
+      </div> */}
+      <div
+        className="w-full h-40 aspect-w-16 aspect-h-9  bg-black
+     sm:aspect-w-4 sm:aspect-h-3 
+     md:aspect-w-16 md:aspect-h-9
+     landscape:aspect-w-21 landscape:aspect-h-9 
+     portrait:aspect-w-4 portrait:aspect-h-3"
+      >
+        <a
+          href="#"
+          class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </a>
       </div>
     </div>
   );
